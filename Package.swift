81240c63ba482f173d03fe34d0149513b2300995
Charles Ku <charlesku915@gmail.com>
2020-05-26 20:06:00 +0800

    Change the swift tool version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["DotEnvVapor"]),
     ],
     dependencies: [
-        .package(name: "Service", url: "https://github.com/vapor/service.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/service.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
